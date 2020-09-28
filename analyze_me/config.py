3cb3110528324525c3a9ae79c5663c5ab8b4001c
Pandra <kassavndra@gmail.com>
2020-09-28 21:21:00 +0900

    modified config.py & layout.html & login.html & view.py

--- analyze_me/config.py
+++ analyze_me/config.py
@@ -1,5 +1,6 @@
 #analyze_me/config.py       2020/9/17   M.O
 
 DEBUG = True
+SECRET_KEY = 'sedret_key'
 USERNAME = 'john'
 PASSWORD = 'due123'
--- analyze_me/views.py
+++ analyze_me/views.py
@@ -3,23 +3,36 @@ from flask import request, redirect, url_for, \
     render_template, flash, session
 from analyze_me import app
 
-
+#トップページ
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#個人情報(personal)ページ
+@app.route('/personal')
+def show_entries():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    return render_template('entries/personal.html')
 
+#ログイン
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
+    error = None
+    if request.method == 'POST':            #ログイン画面入力時
         if request.form['username'] != app.config['USERNAME']:
-            print('ユーザ名が異なります')
+            flash('ユーザ名が異なります')
         elif request.form['password'] != app.config['PASSWORD']:
-            print('パスワードが異なります')
+            flash('パスワードが異なります')
         else:
-            return redirect('/')
-    return render_template('login.html')
+            session['logged_in'] = True
+            flash('ログインしました')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html')    #ログイン画面遷移時、入力エラー時
 
+#ログアウト
 @app.route('/logout')
 def logout():
-    return redirect('/')
+    session.pop('logged_in', None)
+    flash('ログアウトしました')
+    return redirect(url_for('index'))
